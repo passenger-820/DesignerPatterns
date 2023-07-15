2c6768760f58f10aceea382857e46a69b73b0fca
passenger-820 <horizons820@gmail.com>
2023-07-15 22:18:32 +0800

    prototype pattern

---
+++ src/main/java/creational/prototype/deepCopy/recursiveClone/Main.java
@@ -0,0 +1,45 @@
+package creational.prototype.deepCopy.recursiveClone;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //创建原始促销活动
+        final PromotionEvent sourceEvent = createSamplePromotionEvent();
+        System.out.println("sourceEvent.getRules().get(0).getDiscount() = " + sourceEvent.getRules().get(0).getDiscount());
+
+        //创建新的促销活动
+        PromotionEvent newEvent = sourceEvent.clone();
+
+        //设置新的日期
+        newEvent.setStartDate(addDays(sourceEvent.getStartDate(),7));
+        newEvent.setEndDate(addDays(sourceEvent.getEndDate(),7));
+        //设置新的促销策略
+        newEvent.getRules().get(0).setDiscount(newEvent.getRules().get(0).getDiscount() * 100);
+        System.out.println("newEvent.getRules().get(0).getDiscount() = " + newEvent.getRules().get(0).getDiscount());
+    }
+
+    private static PromotionEvent createSamplePromotionEvent() {
+        // 创建示例促销活动
+        List<PromotionRule> rules = Arrays.asList(
+                new PromotionRule("折扣", 0.9, new Product("ss",99,50012)),
+                new PromotionRule("满减", 50, new Product("bb",99,40013))
+        );
+
+        PromotionEvent event = new PromotionEvent("原始促销活动", new Date(), addDays(new Date(), 7),
+                rules
+        );
+        return event;
+    }
+
+    private static Date addDays(Date date, int days) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DATE, days);
+        return calendar.getTime();
+    }
+}
---
+++ src/main/java/creational/prototype/deepCopy/recursiveClone/PromotionRule.java
@@ -0,0 +1,32 @@
+package creational.prototype.deepCopy.recursiveClone;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+// 促销规则
+public class PromotionRule implements Cloneable {
+    private String type;
+    private double discount;
+    private Product product;
+
+    @Override
+    protected PromotionRule clone(){
+        try {
+            //先使用浅拷贝，通过父类的的方法对本身做前拷贝
+            PromotionRule rule = (PromotionRule) super.clone();
+            //本类中存在对Product的引用，因此需要拷贝Product
+            Product copyProduct = (Product) product.clone();
+            //再把产品给到自己，完成深拷贝
+            rule.setProduct(copyProduct);
+            return rule;
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+}
